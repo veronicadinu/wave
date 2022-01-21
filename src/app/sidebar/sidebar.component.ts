d0445c9104bc4a725f814358e84ef4641a330495
Veronica Dinu <veronica@qoffice.ro>
2022-01-21 23:58:02 +0200

    ppp

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { User } from 'firebase/auth';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,19 +12,24 @@ import { User } from 'firebase/auth';
 export class SidebarComponent implements OnInit {
   user: User;
   isAdmin = false;
+  _subscribes: Subscription[] = [];
 
   constructor(public auth: AngularFireAuth, public firestore: AngularFirestore) { }
 
   ngOnInit(): void {
     this.auth.user.subscribe(u => {
+      if(u == null){
+        this.isAdmin = false;
+        this._subscribes.forEach(s => s.unsubscribe());
+      }
       this.user = u;
-      this.firestore.doc(`users/${this.user?.email}`).valueChanges().subscribe((doc: any) => {
+      this._subscribes.push(this.firestore.doc(`users/${this.user?.email}`).valueChanges().subscribe((doc: any) => {
         if (doc?.role == 'admin') {
           this.isAdmin = true;
         } else {
           this.isAdmin = false;
         }
-      });
+      }));
     })
   }
 
